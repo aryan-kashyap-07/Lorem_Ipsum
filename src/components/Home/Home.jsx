ee14620017671ed468f7e616ad8adc58e31ea0c3
aryan-kashyap-07 <aryankashyap3070@gmail.com>
2024-10-06 13:27:39 +0530

    aryan

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import HeroImg from '../../assets/HeroImg.png'
 
 const Home = () => {
   return (
-    <div>
-        
+    <div className='flex relative'>
+        <img src={HeroImg} alt="" className=' w-full h-{397px}' />
+        <p className=' absolute mt-24 ml-20 text-white font-semibold text-6xl'>PMSSS</p>
+        <p className=' absolute mt-48 ml-20 text-white font-semibold text-{18px} font-rubik'>PRADHAN MANTRI SPECIAL SCHOLARSHIP SCHEME FOR J&K AND LADAKH</p>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Header from './components/Header.jsx'
+import Home from './components/Home/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header/>
     <App />
+    <Home/>
   </StrictMode>,
 )
